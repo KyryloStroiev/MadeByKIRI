507a58d7b3e6ca7c1e3d0289f36edeabed9d49e3
KyryloStroiev <stroievkyrylo@gmail.com>
2023-02-09 11:38:24 +0200

    Update Order.cs

--- MadeBuKIRI/Models/Order.cs
+++ MadeBuKIRI/Models/Order.cs
@@ -1,13 +1,35 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
 
 namespace MadeByKIRI.Models;
 
 public class Order
 {
-    public int OrderId { get; set; }
-    public DateTime DateOrder { get; set; }
-    public int BuyersId { get; set; }
-    public Buyers Buyers { get; set; }
-    public int GoodsId { get; set; }
-    public Goods Goods { get; set; }
+	[BindNever]
+	[Key] public int Id { get; set; }
+	[Display(Name = "Enter Name")]
+	[StringLength(50, MinimumLength = 2)]
+	[Required(ErrorMessage = " Довжина не менше 2 символів ")]
+	public string Name { get; set; }
+	[Display(Name = "Enter SurName")]
+	[StringLength(50, MinimumLength = 2)]
+	[Required(ErrorMessage = " Довжина не менше 2 символів ")]
+	public string SurName { get; set; }
+	[Display(Name = "Enter Adress")]
+	[StringLength(int.MaxValue, MinimumLength = 15)]
+	[Required(ErrorMessage = " Довжина не менше 15 символів ")]
+	public string Adress { get; set; }
+	[Display(Name = "Enter phone number")]
+	[DataType(DataType.PhoneNumber)]
+	[StringLength(85, MinimumLength = 8)]
+	[Required(ErrorMessage = " Довжина не менше 8 символів ")]
+	public string Phone { get; set; }
+	[Display(Name = "Enter Email")]
+	[DataType(DataType.EmailAddress)]
+	[StringLength(100, MinimumLength = 5)]
+	[Required(ErrorMessage = " Довжина не менше 5 символів ")]
+	public string Email { get; set; }
+	public DateTime OrderTime { get; set; }
+
+	public List<OrderDetail> OrderDetail { get; set; }
 }
