1abd8698dcc02e71bc60ca0f95f6d774d58b9fed
KyryloStroiev <stroievkyrylo@gmail.com>
2023-02-09 11:39:00 +0200

    Update Program.cs

--- MadeBuKIRI/Program.cs
+++ MadeBuKIRI/Program.cs
@@ -1,3 +1,5 @@
+using KIRI.Domen.Repository;
+using MadeByKIRI.Interfaces;
 using MadeByKIRI.Value;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
@@ -5,14 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 BasePath = builder.Environment.WebRootPath;
 builder.Logging.SetMinimumLevel(LogLevel.Debug);
 builder.Services.AddControllersWithViews();
-builder.Services.AddDistributedMemoryCache();
-
-builder.Services.AddSession(options =>
-{
-	options.IdleTimeout = TimeSpan.FromSeconds(10);
-	options.Cookie.HttpOnly = true;
-	options.Cookie.IsEssential = true;
-});
+builder.Services.AddTransient<IAllGoods, GoodsRepository>();
+builder.Services.AddTransient<IAllOrders, OrdersRepository>();
+//builder.Services.AddTransient<IGoodsCategory, CategoryRepository>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped(sp => ShopCart.GetCart(sp));
 builder.Services.AddDbContext<NewDbContext>(options =>
 options.UseSqlServer(Setting.ConnectionString));
 builder.Services.AddScoped<GoodsRepository>();
@@ -20,9 +19,17 @@ builder.Services
     .AddAuthentication(optional => optional.DefaultScheme
 = CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie();
+builder.Services.AddMvc();
+builder.Services.AddMemoryCache();
+builder.Services.AddSession(options =>
+{
+	options.IdleTimeout = TimeSpan.FromSeconds(300);
+	options.Cookie.HttpOnly = true;
+	options.Cookie.IsEssential = true;
+});
 var app = builder.Build();
 app.UseDeveloperExceptionPage();
-
+app.UseStatusCodePages();
 app.UseHsts();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
