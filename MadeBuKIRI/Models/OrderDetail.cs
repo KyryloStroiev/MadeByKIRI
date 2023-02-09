423a7916c17809b23b4d696298cb63330f91e6df
KyryloStroiev <stroievkyrylo@gmail.com>
2023-02-09 11:38:31 +0200

    Create OrderDetail.cs

---
+++ MadeBuKIRI/Models/OrderDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace MadeByKIRI.Models;
+
+public class OrderDetail
+{
+	[Key] public int Id { get; set; }
+	public int OrderId { get; set; }
+	public int GoodsId { get; set; }
+	public int Price { get; set; }
+	public virtual Goods Goods { get; set; }
+	public virtual Order Order { get; set; }
+}
