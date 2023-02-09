f2d8b65adee8fc7b8e3660733d7e8e67176ea4bf
KyryloStroiev <stroievkyrylo@gmail.com>
2023-02-09 11:36:07 +0200

    Update AdminController.cs

--- MadeBuKIRI/Controllers/AdminController.cs
+++ MadeBuKIRI/Controllers/AdminController.cs
@@ -1,20 +1,11 @@
-﻿using MadeByKIRI.Models;
-using MadeByKIRI.Value;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Diagnostics;
-using System.Security.Claims;
+﻿
+namespace MadeByKIRI.Controllers;
 
-namespace HW35MVC.Controllers
+public class AdminController : Controller
 {
-    public class AdminController : Controller
-    {
-        private readonly ILogger<AdminController> _logger;
-        private NewDbContext _dbContext;
-        private readonly GoodsRepository _repository;
+    private readonly ILogger<AdminController> _logger;
+    private NewDbContext _dbContext;
+    private readonly GoodsRepository _repository;
 
 		 public List<Login> MockUsers = new()
 	     {
@@ -24,162 +15,144 @@ namespace HW35MVC.Controllers
 	     };
 
 		public AdminController(ILogger<AdminController> logger, NewDbContext context, 
-            GoodsRepository repository)
-        {
-            _logger = logger;
-            _dbContext = context;
-            _repository = repository;
-        }
-        [HttpGet]
-        public async Task<ActionResult<List<Goods>>> ShowGoods()
-        {
-			var goods = await _repository.GetGoods();
-           return View(goods);
-		}
-          
-        public async Task<ActionResult<List<Buyers>>> ShowBuyer() =>
-           View(await _dbContext.Buyers.ToListAsync());
-        public async Task<ActionResult<List<Order>>> Admin() =>
-           View(await _dbContext.Order.Include(p => p.Buyers).Include(p => p.Goods).ToListAsync());
-        //public ActionResult ShowUsersDi()
-        //{
-        //	if (User.IsInRole("Admin")) return RedirectToAction("Admin");
-        //	return View();
-        //}
-        [AllowAnonymous]
-        public ActionResult Login() => View();
-        [HttpPost]
-        public async Task<IActionResult> Login(Login login)
-        {
-            
-            var dbUser = MockUsers
-                .FirstOrDefault(userLogin => userLogin.UserName == login.UserName &&
-                                             userLogin.Password == login.Password);
+        GoodsRepository repository)
+    {
+        _logger = logger;
+        _dbContext = context;
+        _repository = repository;
+    }
+    [HttpGet]
+    public async Task<ActionResult<List<Goods>>> ShowGoods()
+    {
+        return View(await _dbContext.Goods.ToListAsync());
+    }
 
-            if (dbUser is not null)
-            {
-                _logger.LogInformation($"User {dbUser.UserName} has signed in");
-                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
-                    new ClaimsPrincipal(new ClaimsIdentity(
-                        new List<Claim>
-                        {
-                        new(ClaimsIdentity.DefaultNameClaimType, dbUser.UserName),
-                        new(ClaimsIdentity.DefaultRoleClaimType, dbUser.Role)
-                        },
-                        "applicationCookie",
-                        ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType))
-                );
-            }
-            else _logger.LogError($"Invalid login {login.UserName} & {login.Password}");
+    public async Task<ActionResult<List<Order>>> ShowBuyer() =>
+       View(await _dbContext.Order.ToListAsync());
+    public async Task<ActionResult<List<OrderDetail>>> Admin() =>
+       View(await _dbContext.OrderDetail.Include(p => p.Order).Include(p=>p.Goods).ToListAsync());
 
-            return RedirectToAction("/Home/Home");
-        }
-        public async Task<ActionResult> Logout()
-        {
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToAction("Home/Home");
-        }
-        [HttpGet]
-        public async Task<ActionResult<Goods>> EditGoods(int Id) =>
-            View(await _dbContext.Goods.FirstOrDefaultAsync(u => u.GoodsId == Id));
-
-        [HttpPost]
-        public async Task<IActionResult> EditGoods (Goods goods)
-        {
-            _dbContext.Entry(await _dbContext.Goods
-                .FirstOrDefaultAsync(dbgoods => dbgoods.GoodsId == goods.GoodsId))
-                .CurrentValues.SetValues(goods);
-            await _dbContext.SaveChangesAsync(); 
-            await UploadImage(goods.GoodsId, Request);
-            return RedirectToAction("ShowGoods");
-        }
-        public ActionResult CreateGoods()
-        {
-            return View();
-        }
-
-
-        [HttpPost]
-        public async Task<IActionResult> CreateGoods(Goods goods)
-        {
-            var newgoods = await _dbContext.Goods
-                .AllAsync(dbgoods => dbgoods.GoodsId != goods.GoodsId)
-                 ? new Goods(goods) : null;
-            _ = _dbContext.Set<Goods>().AddAsync(newgoods!);
-            await _dbContext.SaveChangesAsync();
-
-
-            return RedirectToAction("ShowGoods");
-        }
-        public ActionResult CreateBuyers()
-        {
-            return View();
-        }
-
-
-        [HttpPost]
-        public async Task<IActionResult> CreateBuyers(Buyers buyers)
-        {
-            var newbuyers= await _dbContext.Buyers
-                .AllAsync(dbbuyer => dbbuyer.IDBuyers != buyers.IDBuyers)
-                 ? new Buyers (buyers) : null;
-            _ = _dbContext.Set<Buyers>().AddAsync(newbuyers!);
-            await _dbContext.SaveChangesAsync();
-
-
-            return RedirectToAction("ShowBuyer");
-        }
+    [AllowAnonymous]
+    public ActionResult Login() => View();
+    [HttpPost]
+    public async Task<IActionResult> Login(Login login)
+    {
         
+        var dbUser = MockUsers
+            .FirstOrDefault(userLogin => userLogin.UserName == login.UserName &&
+                                         userLogin.Password == login.Password);
 
-        public async Task<IActionResult> DeleteGoods (int goodsid)
+        if (dbUser is not null)
         {
-            var goods = await _dbContext.Goods
-                 .FirstOrDefaultAsync(dbgoods => dbgoods.GoodsId == goodsid);
-            if (goods !=null)
-            {
-                _dbContext.Goods.Remove(goods);
-                await _dbContext.SaveChangesAsync();
-            }
-            return RedirectToAction("ShowGoods");
+            _logger.LogInformation($"User {dbUser.UserName} has signed in");
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(new ClaimsIdentity(
+                    new List<Claim>
+                    {
+                    new(ClaimsIdentity.DefaultNameClaimType, dbUser.UserName),
+                    new(ClaimsIdentity.DefaultRoleClaimType, dbUser.Role)
+                    },
+                    "applicationCookie",
+                    ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType))
+            );
         }
-        public async Task<IActionResult> DeleteBuyer(int buyerid)
-        {
-            var buyer = await _dbContext.Buyers
-                 .FirstOrDefaultAsync(dbbuyer => dbbuyer.IDBuyers == buyerid);
-            if (buyer != null)
-            {
-                _dbContext.Buyers.Remove(buyer);
-                await _dbContext.SaveChangesAsync();
-            }
-            return RedirectToAction("ShowBuyer");
-        }
-        [HttpGet]
-        public async Task<ActionResult<Buyers>> EditBuyer(int? Id) =>
-          View(await _dbContext.Buyers.FirstOrDefaultAsync(u => u.IDBuyers == Id));
+        else _logger.LogError($"Invalid login {login.UserName} & {login.Password}");
 
-        [HttpPost]
-        public async Task<IActionResult> EditBuyer(int id)
+        return LocalRedirect("~/Home/Home");
+    }
+    public async Task<ActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return LocalRedirect("~/Home/Home");
+    }
+    [HttpGet]
+    public async Task<ActionResult<Goods>> EditGoods(int Id) =>
+        View(await _dbContext.Goods.FirstOrDefaultAsync(u => u.GoodsId == Id));
+
+    [HttpPost]
+    public async Task<IActionResult> EditGoods (Goods goods)
+    {
+        _dbContext.Entry(await _dbContext.Goods
+            .FirstOrDefaultAsync(dbgoods => dbgoods.GoodsId == goods.GoodsId))
+            .CurrentValues.SetValues(goods);
+        await _dbContext.SaveChangesAsync(); 
+        await UploadImage(goods.GoodsId, Request);
+        return RedirectToAction("ShowGoods");
+    }
+    public ActionResult CreateGoods()
+    {
+        return View();
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> CreateGoods(Goods goods)
+    {
+        var newgoods = await _dbContext.Goods
+            .AllAsync(dbgoods => dbgoods.GoodsId != goods.GoodsId)
+             ? new Goods(goods) : null;
+        _ = _dbContext.Set<Goods>().AddAsync(newgoods!);
+        await _dbContext.SaveChangesAsync();
+
+
+        return RedirectToAction("ShowGoods");
+    }
+    public ActionResult CreateBuyers()
+    {
+        return View();
+    }
+
+
+    
+
+    public async Task<IActionResult> DeleteGoods (int goodsid)
+    {
+        var goods = await _dbContext.Goods
+             .FirstOrDefaultAsync(dbgoods => dbgoods.GoodsId == goodsid);
+        if (goods !=null)
         {
-            _dbContext.Entry(await _dbContext.Buyers
-                .FirstOrDefaultAsync(dbbuyer => dbbuyer.IDBuyers == dbbuyer.IDBuyers))
-                .CurrentValues.SetValues(id);
+            _dbContext.Goods.Remove(goods);
             await _dbContext.SaveChangesAsync();
-            return RedirectToAction("ShowBuyer");
         }
-
-        public IActionResult DeleteFile (string filePath)
+        return RedirectToAction("ShowGoods");
+    }
+    public async Task<IActionResult> DeleteBuyer(int orderid)
+    {
+        var order= await _dbContext.Order
+             .FirstOrDefaultAsync(dborder => dborder.Id == orderid);
+        if (order != null)
         {
-            var goodsId = filePath.Split("-").First();
-            var fullpath = $"{BasePath}/images/goods/{filePath}";
-            if(System.IO.File.Exists(fullpath))
-               System.IO.File.Delete(fullpath);
-            return RedirectToAction("EditGoods", new { id = goodsId });
+            _dbContext.Order.Remove(order);
+            await _dbContext.SaveChangesAsync();
         }
+        return RedirectToAction("ShowBuyer");
+    }
+    [HttpGet]
+    public async Task<ActionResult<Order>> EditBuyer(int? Id) =>
+      View(await _dbContext.Order.FirstOrDefaultAsync(u => u.Id== Id));
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [HttpPost]
+    public async Task<IActionResult> EditBuyer(int id)
+    {
+        _dbContext.Entry(await _dbContext.Order
+            .FirstOrDefaultAsync(dbbuyer => dbbuyer.Id == dbbuyer.Id))
+            .CurrentValues.SetValues(id);
+        await _dbContext.SaveChangesAsync();
+        return RedirectToAction("ShowBuyer");
+    }
+
+    public IActionResult DeleteFile (string filePath)
+    {
+        var goodsId = filePath.Split("-").First();
+        var fullpath = $"{BasePath}/images/goods/{filePath}";
+        if(System.IO.File.Exists(fullpath))
+           System.IO.File.Delete(fullpath);
+        return RedirectToAction("EditGoods", new { id = goodsId });
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
