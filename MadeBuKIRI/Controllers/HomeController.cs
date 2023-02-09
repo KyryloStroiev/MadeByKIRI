bf5242c039a8a45e2e5e008c9925589bfda3caae
KyryloStroiev <stroievkyrylo@gmail.com>
2023-02-09 11:36:57 +0200

    Update HomeController.cs

--- MadeBuKIRI/Controllers/HomeController.cs
+++ MadeBuKIRI/Controllers/HomeController.cs
@@ -1,61 +1,57 @@
-﻿using MadeByKIRI.Models;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using System.Security.Claims;
+﻿
+namespace MadeByKIRI.Controllers;
 
-namespace HW35MVC.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private NewDbContext _dbContext;
+    private readonly IAllGoods _allGoods;
+  
+    private readonly ILogger<HomeController> _logger;
+    private NewDbContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger, NewDbContext context)
-        {
-            _logger = logger;
-            _dbContext = context;
-        }
-   
-        public IActionResult Home()
-        {
-            return View();
-        }
+    public HomeController(ILogger<HomeController> logger, NewDbContext context, IAllGoods goodsRep)
+    {
+        _allGoods = goodsRep;
+        _logger = logger;
+        _dbContext = context;
+    }
+  
+    public IActionResult Home()
+    {
+        return View();
+    }
 	
 		public async Task<ActionResult<Goods>> Wallets()
-        {
-            return View(await _dbContext.Goods.ToListAsync());
-        }
-        public async Task<ActionResult<Goods>> Bags()
-        {
-            return View(await _dbContext.Goods.ToListAsync());
-        }
-        public async Task<ActionResult<Goods>> Backpacks()
-        {
-            return View(await _dbContext.Goods.ToListAsync());
-        }
-        public async Task<ActionResult<Goods>> Belts()
-        {
-            return View(await _dbContext.Goods.ToListAsync());
-        }
-        public async Task<ActionResult<Goods>> CardGoods(int id)
-        {
-            return View(await _dbContext.Goods.FirstAsync(goods => goods.GoodsId == id));
-        }
-        public async Task<ActionResult<Goods>> SmallGoods()
-        {
-            return View(await _dbContext.Goods.ToListAsync());
-        }
-        public IActionResult Contacts()
-        {
-            return View();
-        }
+    {
+        return View(await _dbContext.Goods.ToListAsync());
+    }
+    public async Task<ActionResult<Goods>> Bags()
+    {
+        return View(await _dbContext.Goods.ToListAsync());
+    }
+    public async Task<ActionResult<Goods>> Backpacks()
+    {
+        return View(await _dbContext.Goods.ToListAsync());
+    }
+    public async Task<ActionResult<Goods>> Belts()
+    {
+        return View(await _dbContext.Goods.ToListAsync());
+    }
+    public async Task<ActionResult<Goods>> CardGoods(int id)
+    {
+        return View(await _dbContext.Goods.FirstAsync(goods => goods.GoodsId == id));
+    }
+    public async Task<ActionResult<Goods>> SmallGoods()
+    {
+        return View(await _dbContext.Goods.ToListAsync());
+    }
+    public IActionResult Contacts()
+    {
+        return View();
+    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
