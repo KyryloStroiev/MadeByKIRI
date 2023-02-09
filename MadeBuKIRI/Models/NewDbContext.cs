2f1e2efaeaaf022184a4fd2af4de20de3d6ba955
KyryloStroiev <stroievkyrylo@gmail.com>
2023-02-09 11:38:17 +0200

    Update NewDbContext.cs

--- MadeBuKIRI/Models/NewDbContext.cs
+++ MadeBuKIRI/Models/NewDbContext.cs
@@ -6,32 +6,33 @@ namespace MadeByKIRI.Models;
 
 public class NewDbContext : DbContext
 {
-    public DbSet<Buyers> Buyers { get; set; }
+    public DbSet<ShopCartItem> ShopCartItem { get; set; }
     public DbSet<Goods> Goods { get; set; }
+	public DbSet<OrderDetail> OrderDetail { get; set; }
     public DbSet<Order> Order { get; set; }
     
     public NewDbContext(DbContextOptions<NewDbContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        
 
-        modelBuilder.Entity<Order>()
-         .HasKey(t => new { t.BuyersId, t.GoodsId });
+		modelBuilder.Entity<ShopCartItem>()
+					.HasOne(p => p.Goods)
+					.WithOne(p => p.ShopCartItem)
+					.HasForeignKey<ShopCartItem>(b => b.IdItem);
 
-        modelBuilder.Entity<Order>()
-            .HasOne(pt => pt.Buyers)
-            .WithMany(p => p.Order)
-            .HasForeignKey(pt => pt.BuyersId);
-
-        modelBuilder.Entity<Order>()
-            .HasOne(pt => pt.Goods)
-            .WithMany(t => t.Order)
-            .HasForeignKey(pt => pt.GoodsId);
-
-
+		modelBuilder.Entity<OrderDetail>()
+			.HasOne(goods => goods.Goods)
+			.WithOne(goods => goods.OrderDetail)
+			.HasForeignKey<OrderDetail>(goods => goods.GoodsId);
+		modelBuilder.Entity<OrderDetail>()
+			.HasOne(order => order.Order)
+			.WithMany(order => order.OrderDetail)
+			.HasForeignKey(order => order.OrderId);
 
 
-    }
+
+
+	}
 
 }
