5ed87657dcbfafb68dc90156ee69c3b24161f057
KyryloStroiev <stroievkyrylo@gmail.com>
2023-02-09 11:38:12 +0200

    Update GoodsModel.cs

--- MadeBuKIRI/Models/GoodsModel.cs
+++ MadeBuKIRI/Models/GoodsModel.cs
@@ -7,9 +7,9 @@ public class GoodsModel
     public int GoodsId { get; set; }
     public string? NameGoods { get; set; }
     public string? Article { get; set; }
-    public decimal Price { get; set; }
-    public int Quantity { get; set; }
+    public int Price { get; set; }
     public string? Info { get; set; }
-
+	public bool  Available { get; set; }
+    public bool isFavourite { get; set; }
 
 }
