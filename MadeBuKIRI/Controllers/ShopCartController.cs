90f1299a28ae3347deed6c420a7993f512e93fba
KyryloStroiev <stroievkyrylo@gmail.com>
2023-02-09 11:37:07 +0200

    Create ShopCartController.cs

---
+++ MadeBuKIRI/Controllers/ShopCartController.cs
@@ -0,0 +1,54 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace MadeByKIRI.Controllers;
+
+public class ShopCartController : Controller
+{
+	private readonly IAllGoods _goodsRep;
+	private readonly ShopCart _shopCart;
+	private readonly NewDbContext _dbContext;
+
+	public ShopCartController(IAllGoods goodsRep, ShopCart shopCart, NewDbContext dbConext)
+	{
+		_goodsRep = goodsRep;
+		_shopCart = shopCart;
+		_dbContext = dbConext;
+	}
+	public ViewResult Index()
+	{
+		var items = _shopCart.GetShopItem();
+		_shopCart.ListShopItem = items;
+
+		var obj = new ShopCartViewModel
+		{
+			shopCart = _shopCart
+		};
+		return View(obj);
+	}
+    
+    public RedirectToActionResult AddToCarts(int goodsId)
+	{
+		var item = _goodsRep.Goods.FirstOrDefault(c => c.GoodsId == goodsId);
+		if (item != null)
+		{
+			_shopCart.AddToCarts(item);
+		}
+		return RedirectToAction ("Index");
+	}
+	public async Task<IActionResult> DeleteGoods(int goodsid)
+	{
+		var goods = await _dbContext.ShopCartItem
+			 .FirstOrDefaultAsync(dbgoods => dbgoods.IdItem == goodsid);
+		if (goods != null)
+		{
+			_dbContext.ShopCartItem.Remove(goods);
+			await _dbContext.SaveChangesAsync();
+		}
+		return RedirectToAction("Index");
+	}
+
+
+}
+
