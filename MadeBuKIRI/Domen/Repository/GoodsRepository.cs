5a03732100e67c89ee40163a7a0ef11f930e7c45
KyryloStroiev <stroievkyrylo@gmail.com>
2023-02-09 11:37:10 +0200

    Create GoodsRepository.cs

---
+++ MadeBuKIRI/Domen/Repository/GoodsRepository.cs
@@ -0,0 +1,19 @@
+﻿
+namespace KIRI.Domen.Repository;
+
+public class GoodsRepository:  IAllGoods
+{
+	private readonly NewDbContext _appDBContext;
+
+	public GoodsRepository(NewDbContext appDBContext)
+	{
+		_appDBContext = appDBContext;
+	}
+
+	public IEnumerable<Goods> Goods => _appDBContext.Goods;
+
+
+
+
+	public Goods GetObjectGoods(int goodsId) => _appDBContext.Goods.FirstOrDefault(p => p.GoodsId == goodsId);
+}
