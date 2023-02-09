225792449701edbf43caa494cca32b1734ec3c8e
KyryloStroiev <stroievkyrylo@gmail.com>
2023-02-09 11:38:56 +0200

    Create ShopCartItem.cs

---
+++ MadeBuKIRI/Models/ShopCartItem.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MadeByKIRI.Models;
+
+public class ShopCartItem
+{
+	[Key] public int IdItem{ get; set; }
+	public Goods Goods { get; set; }
+	public int Price { get; set; }
+	public string ShopCartId { get; set; }
+}
