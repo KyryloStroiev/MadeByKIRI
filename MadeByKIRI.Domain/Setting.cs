06143442a62b86eeb2bab5035057ddb636b7fafa
KyryloStroiev <stroievkyrylo@gmail.com>
2023-02-13 13:42:58 +0200

    Create Setting.cs

---
+++ MadeByKIRI.Domain/Setting.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace MadeByKIRI.Models;
+
+public static class Setting
+{
+    public static string ConnectionString => new SqlConnectionStringBuilder
+    {
+        DataSource = "localhost",
+        InitialCatalog = "ShopKiri",
+        IntegratedSecurity = true,
+        TrustServerCertificate = true
+    }.ConnectionString;
+    public static JsonSerializerOptions SerializerOptions = new()
+    {
+        ReferenceHandler = ReferenceHandler.IgnoreCycles
+    };
+}
+
