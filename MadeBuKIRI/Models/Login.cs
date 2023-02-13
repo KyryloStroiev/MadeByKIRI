5d789d098d511f045b7c272f812218f777a3e176
KyryloStroiev <stroievkyrylo@gmail.com>
2023-02-13 13:31:41 +0200

    Update Login.cs

--- MadeBuKIRI/Models/Login.cs
+++ MadeBuKIRI/Models/Login.cs
@@ -4,6 +4,7 @@ public class Login
 {
     public int Id { get; set; } 
     public string UserName { get; set; }
+    [DataType(DataType.Password)]
     public string Password { get; set; }
     public string Role { get; set; }
 }
