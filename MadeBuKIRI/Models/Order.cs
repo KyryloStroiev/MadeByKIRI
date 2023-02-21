728bc669aaeae12cb8853562e1b95d1c2b3a2a8e
Kyrylo <112381397+KyryloStroiev@users.noreply.github.com>
2023-02-21 13:11:04 +0200

    Update Order.cs

--- MadeBuKIRI/Models/Order.cs
+++ MadeBuKIRI/Models/Order.cs
@@ -8,27 +8,21 @@ public class Order
 	[BindNever]
 	[Key] public int Id { get; set; }
 	[Display(Name = "Enter Name")]
-	[StringLength(50, MinimumLength = 2)]
-	[Required(ErrorMessage = " Довжина не менше 2 символів ")]
-	public string Name { get; set; }
+    [StringLength(50, MinimumLength = 2)]
+    public string Name { get; set; }
 	[Display(Name = "Enter SurName")]
 	[StringLength(50, MinimumLength = 2)]
-	[Required(ErrorMessage = " Довжина не менше 2 символів ")]
 	public string SurName { get; set; }
 	[Display(Name = "Enter Adress")]
 	[StringLength(int.MaxValue, MinimumLength = 15)]
-	[Required(ErrorMessage = " Довжина не менше 15 символів ")]
 	public string Adress { get; set; }
 	[Display(Name = "Enter phone number")]
 	[DataType(DataType.PhoneNumber)]
-	[StringLength(85, MinimumLength = 8)]
-	[Required(ErrorMessage = " Довжина не менше 8 символів ")]
+	[StringLength(50, MinimumLength = 5)]
 	public string Phone { get; set; }
-
 	[Display(Name = "Enter Email")]
 	[DataType(DataType.EmailAddress)]
 	[StringLength(100, MinimumLength = 5)]
-	[Required(ErrorMessage = " Довжина не менше 5 символів ")]
 	public string Email { get; set; }
 	public DateTime OrderTime { get; set; }
 
