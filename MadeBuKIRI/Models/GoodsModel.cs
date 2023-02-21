d332929cecc6ff81bd368b4c5f774d8c7244dad1
Kyrylo <112381397+KyryloStroiev@users.noreply.github.com>
2023-02-21 13:10:48 +0200

    Update GoodsModel.cs

--- MadeBuKIRI/Models/GoodsModel.cs
+++ MadeBuKIRI/Models/GoodsModel.cs
@@ -5,7 +5,7 @@ namespace MadeByKIRI.Models;
 public class GoodsModel
 {
     public int GoodsId { get; set; }
-    public string? NameGoods { get; set; }
+    public string NameGoods { get; set; }
     public string? Article { get; set; }
     public int Price { get; set; }
     public string? Info { get; set; }
