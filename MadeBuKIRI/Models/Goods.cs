15b82874e2fecea5f9b95d392ad66bfcbf58f7fa
Kyrylo <112381397+KyryloStroiev@users.noreply.github.com>
2023-02-21 13:10:45 +0200

    Update Goods.cs

--- MadeBuKIRI/Models/Goods.cs
+++ MadeBuKIRI/Models/Goods.cs
@@ -4,8 +4,9 @@ public class Goods : GoodsModel
 {
 	public virtual List<OrderDetail> OrderDetail { get; set; }
 	public virtual ShopCartItem ShopCartItem { get; set; }
+    //public virtual List<Category> Category { get; set; }
 
-	public Goods() { }
+    public Goods() { }
 
     public Goods (GoodsModel goodsmodel)
     {
